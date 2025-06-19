1a8c30100d00a0d8eabfe6b623cb6ba5ddd73d53
korenvak <koren96@gmail.com>
2025-06-19 20:24:48 +0300

    Rename PyInstaller spec file

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ REM install dependencies
 pip install -r requirements.txt
 
 REM build the EXE
-pyinstaller panel_app.spec --clean
+pyinstaller pyinstaller.spec --clean
 
 echo Build complete!
 pause
